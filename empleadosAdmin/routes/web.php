2d6c4498aa6d0e809a997e30b24a4ab10d03f666
Angelo <atapial@unicca.edu.co>
2024-08-17 18:57:13 -0500

    Cambio dos

--- empleadosAdmin/routes/web.php
+++ empleadosAdmin/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use app\Http\Controllers\EmpleadoController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/empleado', function () {
+    return view('empleado.index');
 });
